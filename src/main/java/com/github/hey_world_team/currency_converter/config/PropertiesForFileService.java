65bb4389fc9e751b890e5198ccf89ad3acd8ddaf
dmitry.ishmitov <dmitry.ishmitov@reltio.com>
2022-05-28 10:40:26 +0300

    dishmitov-sprint-4 added javadoc comments to PropertiesForFileService

--- src/main/java/com/github/hey_world_team/currency_converter/config/PropertiesForFileService.java
+++ src/main/java/com/github/hey_world_team/currency_converter/config/PropertiesForFileService.java
@@ -4,14 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * @author artemnoskov
+ * @date 5/13/22
+ * Класс для введения свойств приложения
+ */
 @Configuration
 @PropertySource("classpath:application.properties")
-public class PropertiesForFileService {              // класс для введения свойств приложения
-    @Value("${filepath}")                            // аннотация для конфигурации свойства хранения файла
-    private String path;                             // переменная path доступная только для данного класса
+@SuppressWarnings("all")
+public class PropertiesForFileService {              //
 
+    /**
+     * Путь до папки с конфигурационными файлами
+     */
+    @Value("${filepath}")
+    private String path;
 
-    public String getPath() {                        // метод для получения пути хранения  файла
-        return path;                                 // метод возвращает переменную path (return-оператор перехода)
+    /**
+     * @return путь хранения  файла
+     */
+    public String getPath() {
+        return path;
     }
 }
